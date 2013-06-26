2a39f640acd8999f26a028e373560ca79a0bb389
g3 <info@g3.mu>
2013-06-26 13:28:23 +0900

    init処理を変更(フレームワークをサブモジュールとして扱う)

--- core/classes/task/coretask.php
+++ core/classes/task/coretask.php
@@ -118,4 +118,46 @@ class Task_Coretask extends Task
 			$error = print_r($error,true);
 		echo $error."\n";
 	}
+	public static function init()
+	{
+		if(func_num_args() == 0){
+			echo "プロジェクト名を指定して下さい\n";
+			exit;
+		}
+		$name = func_get_arg(0);
+		echo "プロジェクト $name を作成します\n";
+		if(file_exists($name)){
+			echo "ディレクトリ $name はすでに存在しています\n";
+			exit;
+		}
+		if(mkdir($name) === false){
+			echo "ディレクトリ $name が作成できませんでした\n";
+			exit;
+		}
+		if(chdir($name) === false){
+			echo "ディレクトリ $name に移動出来ません\n";
+			exit;
+		}
+		/*
+		if(symlink(FWPATH, FWNAME) === false){
+			echo "フレームワーク ".FWPATH." のシンボリックリンクが作成できませんでした\n";
+			exit;
+		}
+		*/
+		
+		// スケルトンのコピー
+		$cmd = "cp -ar ".FWPATH."skel/* ./";
+		passthru($cmd);
+		
+		// Git 初期化
+		passthru("git init");
+		// フレームワークをサブモジュールとして追加
+		passthru("git submodule add ~/repo/mk1.git mk1");
+		
+		// mkコマンドのリンク
+		if(symlink('./mk1/mk.php', 'mk') === false){
+			echo "mkコマンドのシンボリックリンクが作成できませんでした\n";
+			exit;
+		}
+	}
 }
