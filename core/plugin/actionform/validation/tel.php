9779e138816729be63d35a8838d97e48ab385616
Nomoto <masataka@g3.mu>
2013-08-17 17:35:24 +0900

    validation/telで空白を許すよう変更

--- core/plugin/actionform/validation/tel.php
+++ core/plugin/actionform/validation/tel.php
@@ -4,6 +4,6 @@
  */
 return function($value)
 {
-	if( ! preg_match('/^0[0-9]+-[-0-9]+[0-9]$/',$value) )
+	if( strlen($value) > 0 && ! preg_match('/^0[0-9]+-[-0-9]+[0-9]$/',$value) )
 		throw new ValidateErrorException('形式が違います');
 };
