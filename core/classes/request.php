2f6b01eee7be40b2dd9e3bdd06d6b574857fc718
Nomoto <masataka@g3.mu>
2013-11-19 04:08:43 +0900

    コントローラで実行するメソッドのパラメータをActioformにもセットする

--- core/classes/request.php
+++ core/classes/request.php
@@ -20,11 +20,22 @@ class Request
 	{
 		$route_class_name = Config::get('class.route','Route');
 		list($controller_name,$controller_method_name,$controller_method_options) = $route_class_name::get_controller($this->uri);
-		Log::debug("[$route_class_name] controller = $controller_name / method = $controller_method_name");
+		Log::coredebug("[$route_class_name] controller = $controller_name / method = $controller_method_name");
 		
 		$controller = new $controller_name(array(
 			'request' => $this,
 		));
+		
+		// 実行するメソッドのパラメータをActioformにもセットする
+		$af = Actionform::instance();
+		$controller_method_reflection = new ReflectionMethod($controller,$controller_method_name);
+		$controller_method_parameters = $controller_method_reflection->getParameters();
+		foreach($controller_method_options as $option_key => $option_value){
+			if(isset($controller_method_parameters[$option_key])){
+				$af->set($controller_method_parameters[$option_key]->name, $option_value);
+			}
+		}
+		
 		$controller_return_var = call_user_func_array(array($controller,$controller_method_name),$controller_method_options);
 		
 		if($controller_return_var === NULL)
