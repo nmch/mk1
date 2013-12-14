1f871253c3a8eaadc9ce7e8fa83ce614cd8ef6af
Masataka Nomoto <m-nomoto@ct-lab.jp>
2013-12-14 09:02:06 +0900

    validation emailで空文字列に対応

--- core/plugin/actionform/validation/email.php
+++ core/plugin/actionform/validation/email.php
@@ -1,6 +1,8 @@
 <?
 return function($value)
 {
-	if( ! preg_match('/^[^@]+@.+\..+/',$value) )
-		throw new ValidateErrorException('形式が違います');
+	if( $value ){
+		if( ! preg_match('/^[^@]+@.+\..+/',$value) )
+			throw new ValidateErrorException('形式が違います');
+	}
 };
