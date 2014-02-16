24fcdc5d3c48b7eb5c4c54001ac9a8faa68e26d1
Masataka Nomoto <m-nomoto@ct-lab.jp>
2014-02-17 02:46:05 +0900

    電話番号バリデーションを修正

--- core/plugin/actionform/validation/tel.php
+++ core/plugin/actionform/validation/tel.php
@@ -4,6 +4,7 @@
  */
 return function($value)
 {
-	if( strlen($value) > 0 && ! preg_match('/^0[0-9]+-[-0-9]+[0-9]$/',$value) )
-		throw new ValidateErrorException('形式が違います');
+	if( strlen($value) > 0 && ! preg_match('/^0[0-9]+-?[0-9]+-?[0-9]+$/',$value) ){
+		throw new ValidateErrorException("市外局番から0NN-NNN-NNNNの形式で入力して下さい(例:03-1234-5678)");
+	}
 };
