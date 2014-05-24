5a29d5d7119282fbf6dac3ce15ee2dfa6c80f5a2
Masataka Nomoto <m-nomoto@hako-net.co.jp>
2014-05-24 17:01:20 +0900

    date validation追加

---
+++ core/plugin/actionform/validation/date.php
@@ -0,0 +1,16 @@
+<?
+/**
+ * 日付(YYYY-MM-DDまたはYYYY/MM/DD形式)
+ */
+return function($value)
+{
+	if( ! $value ){
+		return $value;
+	}
+	if(preg_match('#^([0-9]+)[-/]([0-9]+)[-/]([0-9]+)$#',$value,$match)){
+		if(checkdate($match[2],$match[3],$match[1])){
+			return $value;
+		}
+	}
+	throw new ValidateErrorException('形式が違います');
+};
