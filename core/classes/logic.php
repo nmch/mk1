a8e98185a58c2492efa359b572ce713d1daa60db
Masataka Nomoto <m-nomoto@ct-lab.jp>
2014-08-12 08:16:51 +0900

    Logicにbefore()/after()追加

--- core/classes/logic.php
+++ core/classes/logic.php
@@ -1,7 +1,17 @@
-<?
+<?php
+/**
+ * ロジック
+ */
 class Logic
 {
 	function __construct()
 	{
+		$this->before();
 	}
+	function __destruct()
+	{
+		$this->after();
+	}
+	function before() {}
+	function after() {}
 }
