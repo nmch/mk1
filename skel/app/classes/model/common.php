9f55f4f6f2a201b5131b74c431b4f6204a8dc596
Masataka Nomoto <m-nomoto@ct-lab.jp>
2014-08-24 21:20:03 +0900

    skel修正

--- skel/app/classes/model/common.php
+++ skel/app/classes/model/common.php
@@ -13,6 +13,8 @@ class Model_Common extends Model
 	protected static $_primary_key = '__primary_key__';
 	protected static $_properties = [
 	];
+	public static $_join = [
+	];
 	protected static $_conditions = [
 		[
 			'label'		=> 'order_by',
