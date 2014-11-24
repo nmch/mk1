e885dc6d68b48d5193eb70d685587e2324db4a7c
Masataka Nomoto <m-nomoto@ct-lab.jp>
2014-11-24 10:55:13 +0900

    smarty_modifier_mb_truncateでmb_strimwidth()を使うよう修正

--- core/plugin/Smarty/modifier.mb_truncate.php
+++ core/plugin/Smarty/modifier.mb_truncate.php
@@ -1,10 +1,9 @@
-<?
-function smarty_modifier_mb_truncate($string, $length = 80, $etc = '...') {
-    if ($length == 0) {return '';}
+<?php
+function smarty_modifier_mb_truncate($string, $length = 80, $etc = '...')
+{
+	if( $length == 0 ){
+		return '';
+	}
 
-    if (mb_strlen($string) > $length) {
-        return mb_substr($string, 0, $length).$etc;
-    } else {
-        return $string;
-    }
-}?>
+	return mb_strimwidth($string, 0, $length, $etc);
+}
