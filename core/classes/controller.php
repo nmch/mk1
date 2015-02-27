154054410a2ea18e31a6980a2c21f412100bdacf
Masataka Nomoto <m-nomoto@ct-lab.jp>
2015-02-27 15:21:17 +0900

    Controller::before_execute(), after_execute()追加

--- core/classes/controller.php
+++ core/classes/controller.php
@@ -18,21 +18,30 @@ class Controller
 		$this->before();
 	}
 
-	function before()
-	{
-	}
+	function before() { }
 
 	function __destruct()
 	{
 		$this->after();
 	}
 
-	function after()
-	{
-	}
+	function after() { }
 
 	function execute($name, array $arguments = [])
 	{
-		return call_user_func_array([$this, $name], $arguments);
+		if( method_exists($this, 'before_execute') ){
+			call_user_func_array([$this, 'before_execute'], [$name, $arguments]);
+		}
+
+		$r = call_user_func_array([$this, $name], $arguments);
+
+		if( method_exists($this, 'after_execute') ){
+			call_user_func_array([$this, 'after_execute'], [$r, $name, $arguments]);
+		}
+
+		return $r;
 	}
+
+//	function before_execute($name, array $arguments = []) { }
+//	function after_execute($r, $name, array $arguments = []) { }
 }
