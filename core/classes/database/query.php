ca22b1af9b2f542c3366b7bd134b4b6a74819f99
Masataka Nomoto <m-nomoto@ct-lab.jp>
2015-06-30 16:35:12 +0900

    Database_Query::where()の第一引数にDatabase_Expressionを渡せるよう変更

--- core/classes/database/query.php
+++ core/classes/database/query.php
@@ -197,7 +197,7 @@ class Database_Query
 
 	function build_where()
 	{
-		//Log::coredebug($this->_query_where,$this->_query_values);
+		Log::coredebug($this->_query_where,$this->_query_values);
 		$last_condition = null;
 
 		$sql = '';
@@ -221,6 +221,9 @@ class Database_Query
 					}
 
 					$sql .= ')';
+				}
+				elseif( ($exp = Arr::get($condition,0)) instanceof Database_Expression ){
+					$sql .= ' '.(string)$exp;
 				}
 				else{
 					if( ! empty($sql) AND $last_condition !== '(' ){
