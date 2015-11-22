9bb0a45d149fc35f98254648ee71e7705ad38134
NOMOTO Masataka <nomoto@bbjoker.com>
2015-11-23 04:06:18 +0900

    empty_or_number_format()がdecimal引数をサポート

--- core/plugin/Smarty/modifier.empty_or_number_format.php
+++ core/plugin/Smarty/modifier.empty_or_number_format.php
@@ -1,5 +1,5 @@
 <?php
-function smarty_modifier_empty_or_number_format($value)
+function smarty_modifier_empty_or_number_format($value, $decimals = 0)
 {
-	return strlen($value) ? number_format($value) : '';
+	return strlen($value) ? number_format($value, $decimals) : '';
 }
