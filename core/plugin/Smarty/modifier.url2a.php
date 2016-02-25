8000941366147e2ed9224be99679e5989a95cfad
NOMOTO Masataka <nomoto@bbjoker.com>
2016-02-26 00:12:31 +0900

    url2aバグ修正

--- core/plugin/Smarty/modifier.url2a.php
+++ core/plugin/Smarty/modifier.url2a.php
@@ -2,7 +2,8 @@
 function smarty_modifier_url2a($content)
 {
 	if( preg_match_all('#(http(s)?://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?)#', $content, $match) ){
-		foreach($match[1] as $url){
+		$targets = array_unique($match[1]);
+		foreach($targets as $url){
 			$content = str_replace($url, '<A href="' . $url . '" target="_blank">' . $url . '</A>', $content);
 		}
 	}
