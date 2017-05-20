de522fa8ab834830c41751be6152f16678b77d08
NOMOTO Masataka <nomoto@bbjoker.com>
2017-05-21 01:50:00 +0900

    mod Masterdetail Logic

--- core/classes/logic/masterdetail/common.php
+++ core/classes/logic/masterdetail/common.php
@@ -11,7 +11,7 @@ trait Logic_Masterdetail_Common
 {
 	private function get_base_class_name()
 	{
-		$current_class_name = get_class();
+		$current_class_name = get_called_class();
 		if( ! preg_match('/^Controller_(.+)$/', $current_class_name, $match) ){
 			throw new Exception('unexpected class name');
 		}
