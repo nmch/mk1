307fe24cb05ba4a69cdb2720b053ed6f28fdc452
NOMOTO Masataka <nomoto@bbjoker.com>
2017-10-12 11:20:11 +0900

    Response_File修正

--- core/classes/response_file.php
+++ core/classes/response_file.php
@@ -3,17 +3,17 @@
 class Response_File extends Response
 {
 	const CONVERT_ENCODING_METHOD_ONMEMORY = 'onmemory';
-
+	
 	protected $filepath;
 	protected $filename;
 	protected $do_not_unlink;
 	protected $unlink_targets = [];
 	protected $mime_type      = 'application/octet-stream';
-
+	
 	function __construct($filepath, $filename, $do_not_unlink = false, array $headers = [])
 	{
 		Log::coredebug("[response file] filename=$filename");
-
+		
 		if( ! file_exists($filepath) ){
 			//			throw new Exception("file not found");
 			throw new HttpNotFoundException;
@@ -24,40 +24,42 @@ class Response_File extends Response
 		if( ! is_readable($filepath) ){
 			throw new Exception("cannot read file");
 		}
-
+		
 		$this->filepath         = $filepath;
 		$this->headers          = $headers;
 		$this->filename         = $filename;
 		$this->do_not_unlink    = $do_not_unlink;
 		$this->unlink_targets[] = $filepath;
 	}
-
+	
 	function convert_encoding($to, $from, $method = 'onmemory')
 	{
 		$tmpfile = tempnam(sys_get_temp_dir(), 'CNV');
 		file_put_contents($tmpfile, mb_convert_encoding(file_get_contents($this->filepath), $to, $from));
 		$this->unlink_targets[] = $tmpfile;
 		$this->filepath         = $tmpfile;
-
+		
 		return $this;
 	}
-
-	function set_mime_type($mime_type):Response_File
+	
+	function set_mime_type($mime_type): Response_File
 	{
 		$this->mime_type = $mime_type;
-
+		
 		return $this;
 	}
-
+	
 	function send()
 	{
 		if( $this->before() ){
 			header('Content-Type: ' . $this->mime_type);
-			header('Content-Disposition: attachment; filename=' . $this->filename);
+			$enc = '=?utf-8?B?' . base64_encode($this->filename) . '?=';
+			header('Content-Disposition: attachment; filename="' . $enc . '"');
+			//header('Content-Disposition: attachment; filename=' . $this->filename);
 			header('Content-Transfer-Encoding: binary');
 			header('Content-Length: ' . filesize($this->filepath));
 			readfile($this->filepath);
-
+			
 			if( ! $this->do_not_unlink ){
 				foreach($this->unlink_targets as $target){
 					if( is_writable($target) ){
