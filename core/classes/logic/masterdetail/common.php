e3bacd0aa07e152dffe7e02c832cc4790adb38dc
NOMOTO Masataka <nomoto@bbjoker.com>
2017-11-21 14:00:40 +0900

    bug fix

--- core/classes/logic/masterdetail/common.php
+++ core/classes/logic/masterdetail/common.php
@@ -11,7 +11,7 @@ trait Logic_Masterdetail_Common
 {
 	private function get_base_class_name()
 	{
-		$current_full_class_name = get_class();
+		$current_full_class_name = get_called_class();
 		$current_class_name      = \Mk::strip_namespace($current_full_class_name);
 		
 		if( ! preg_match('/^Controller_(.+)$/', $current_class_name, $match) ){
