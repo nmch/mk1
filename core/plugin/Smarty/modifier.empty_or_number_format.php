c9d873c5b7f39ba3b44b7f04b8ee7a987367de49
NOMOTO Masataka <nomoto@bbjoker.com>
2018-07-07 20:59:57 +0900

    Smartyフィルタempty_or_number_format引数追加

--- core/plugin/Smarty/modifier.empty_or_number_format.php
+++ core/plugin/Smarty/modifier.empty_or_number_format.php
@@ -1,5 +1,5 @@
 <?php
-function smarty_modifier_empty_or_number_format($value, $decimals = 0)
+function smarty_modifier_empty_or_number_format($value, $decimals = 0, $dec_point = '.', $thousands_sep = ',')
 {
-	return strlen($value) ? number_format($value, $decimals) : '';
+	return strlen($value) ? number_format($value, $decimals, $dec_point, $thousands_sep) : '';
 }
