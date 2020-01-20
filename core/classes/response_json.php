a87301edc011e4d6c0682f5aa45b9d13bf862948
NOMOTO Masataka <nomoto@bbjoker.com>
2020-01-20 23:55:22 +0900

    Response_Jsonでレスポンスボディがnullの際に空文字列で返すよう変更

--- core/classes/response_json.php
+++ core/classes/response_json.php
@@ -4,7 +4,7 @@ class Response_Json extends Response
 {
 	function __construct($data = [], $status = 200, array $headers = [])
 	{
-		$data = Mk::json_encode($data);
+		$data = ($data === null) ? null : Mk::json_encode($data);
 		$r    = parent::__construct($data, $status, $headers);
 		
 		$this->set_header('Content-Type', Config::get('response_json.content-type', 'application/json; charset=utf-8'));
