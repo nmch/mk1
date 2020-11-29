2c8ea73976cc79b4daa0506362c21cf34dc2b292
NOMOTO Masataka <nomoto@bbjoker.com>
2020-11-29 11:36:49 +0900

    update coretask

--- core/classes/task/coretask.php
+++ core/classes/task/coretask.php
@@ -191,9 +191,8 @@ class Task_Coretask extends Task
 		// Git 初期化
 		passthru("git init");
 		// フレームワークをサブモジュールとして追加
-		passthru("git submodule add https://gitlab+deploy-token-1:ExM_1YKUfFgTSszLsbxf@gitlab.hkdv.net/nomoto/mk1.git");
+		passthru("git submodule add git@github.com:nmch/mk1.git");
 		passthru("git submodule update --init");
-		passthru("cd mk1; git remote set-url --push origin ssh://git@gitlab.hkdv.net:2200/nomoto/mk1.git");
 		/*
 		// mkコマンドのリンク
 		if(symlink('./mk1/mk.php', 'mk') === false){
