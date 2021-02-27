b710ce5cfb9558c8018f4fc5d8c39822b8fbcc4a
NOMOTO Masataka <nomoto@bbjoker.com>
2021-02-27 22:18:55 +0900

    Responseのデフォルトヘッダ追加

--- core/config/response.php
+++ core/config/response.php
@@ -4,5 +4,6 @@ return [
 		'X-FRAME-OPTIONS'        => 'SAMEORIGIN',
 		'X-Content-Type-Options' => 'nosniff',
 		'X-XSS-Protection'       => '1; mode=block',
+		'Referrer-Policy'        => 'no-referrer-when-downgrade',
 	],
 ];
