573b9965b1d67167a319b6dafa4309fd7e94b090
NOMOTO Masataka <nomoto@bbjoker.com>
2022-09-21 17:37:58 +0900

    reformat modifier.convert_kanahan.php

--- core/plugin/Smarty/modifier.convert_kanahan.php
+++ core/plugin/Smarty/modifier.convert_kanahan.php
@@ -1,7 +1,8 @@
 <?php
+
 function smarty_modifier_convert_kanahan($string)
 {
-	$string = mb_convert_kana($string, 'k');
+    $string = mb_convert_kana($string, 'k');
 
-	return $string;
+    return $string;
 }
