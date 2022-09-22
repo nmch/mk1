540826a6f34a926059375d3dc66a561a0fc654c0
NOMOTO Masataka <nomoto@bbjoker.com>
2022-09-23 08:11:31 +0900

    reformat session/driver/dynamodb.php

--- core/classes/session/driver/dynamodb.php
+++ core/classes/session/driver/dynamodb.php
@@ -9,19 +9,19 @@
 
 class Session_Driver_Dynamodb
 {
-	static function instance(array $driver_config): \Aws\DynamoDb\SessionHandler
-	{
-		$dynamodb        = new Dynamodb($driver_config['connection'] ?? null);
-		$dynamodb_client = $dynamodb->client;
-		
-		$table_name = $driver_config['table'] ?? 'sessions';
-		
-		$params  = $driver_config['handler_config'] ?? [];
-		$params  += [
-			'table_name' => $table_name,
-		];
-		$handler = \Aws\DynamoDb\SessionHandler::fromClient($dynamodb_client, $params);
-		
-		return $handler;
-	}
+    static function instance(array $driver_config): \Aws\DynamoDb\SessionHandler
+    {
+        $dynamodb = new Dynamodb($driver_config['connection'] ?? null);
+        $dynamodb_client = $dynamodb->client;
+
+        $table_name = $driver_config['table'] ?? 'sessions';
+
+        $params = $driver_config['handler_config'] ?? [];
+        $params += [
+            'table_name' => $table_name,
+        ];
+        $handler = \Aws\DynamoDb\SessionHandler::fromClient($dynamodb_client, $params);
+
+        return $handler;
+    }
 }
