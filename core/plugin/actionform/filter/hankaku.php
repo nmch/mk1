c7b2eb302dcf7bfe72d40616334fb7d49a1b12df
NOMOTO Masataka <nomoto@bbjoker.com>
2022-10-08 10:51:12 +0900

    PHP8.1対応

--- core/plugin/actionform/filter/hankaku.php
+++ core/plugin/actionform/filter/hankaku.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * 全角の英数字とスペースを半角変換
  *
  * @return string
  */
 return function ($value) {
-	$new_value = mb_convert_kana($value, "sa");
+    $new_value = mb_convert_kana($value ?? '', "sa");
 
-	return $new_value;
+    return $new_value;
 };
